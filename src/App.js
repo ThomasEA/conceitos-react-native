b24a1b879f5a9648c9f05ed920595980a678bc59
Everton Thomas <everton@automatizesistemas.com.br>
2020-06-19 20:26:21 -0300

    Challenge done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import {
   SafeAreaView,
@@ -11,45 +12,88 @@ import {
 } from "react-native";
 
 export default function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories')
+      .then( response => {
+        console.log(response.data);
+        setRepositories(response.data);
+      })
+      .catch( reason => {
+        console.log(`Error: ${reason}`);
+      })
+  }, [])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    api.post(`repositories/${id}/like`)
+      .then( response => {
+        if (response.status === 200)
+          applyLike(id);
+      })
+      .catch( reason => {
+        console.log(`Error: ${reason}`);
+      });
+  }
+
+  function applyLike(id) {
+    let newRepositories = [...repositories];
+
+    const repositoryIndex = newRepositories.findIndex(r => r.id === id);
+
+    if (repositoryIndex >= 0) {
+      const oldRepository = newRepositories[repositoryIndex];
+
+      newRepositories[repositoryIndex] = {...newRepositories[repositoryIndex], likes: newRepositories[repositoryIndex].likes + 1};
+
+      setRepositories(newRepositories);
+    }
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList 
+          style={styles.container}
+          data={repositories}
+          keyExtractor={item => item.id}
+          renderItem={({item: repository}) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+              
+                <View style={styles.techsContainer}>
+                {repository.techs.map( tech => (
+                  <Text key={tech} style={styles.tech}>
+                    {tech}
+                  </Text>
+                ))}
+                </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  {repository.likes} curtidas
+                </Text>
+              </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${repository.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}>
+
+        </FlatList>
       </SafeAreaView>
     </>
   );
